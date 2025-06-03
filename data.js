5b4cd3ee9eb9db27e304e1131818b4262096c907
MrKazzanek <161287375+MrKazzanek@users.noreply.github.com>
2025-06-03 18:36:34 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,14 @@
 
 const wallpapersData = [
    {
+    id: 62,
+    author: "MineWallpapers",
+    image_thumb: "https://i.ibb.co/k2509dF6/Zrzut-ekranu-56_thumb.png", // Sprawdź, czy ta ścieżka jest poprawna!
+    image_full: "https://i.ibb.co/k2509dF6/Zrzut-ekranu-56.png",
+    download_page_url: "https://www.mediafire.com/file/cosb8j2ft14qsm0/MineWallpapers-Cloudcraft.zip/file",
+    is_new: true
+  },
+   {
     id: 61,
     author: "Bluzers",
     image_thumb: "https://i.ibb.co/jkGP0hq7/obj-matte-The-End-Mansion-Flat-Bluzers-minecraft_thumb.png", // Sprawdź, czy ta ścieżka jest poprawna!
